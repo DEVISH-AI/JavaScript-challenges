b5b7c36b2ba1aa2ea83ec82ac2033f284eb663e3
Hashmeena_Farhan <devish8463@gmail.com>
2025-08-25 02:29:19 +0500

    Add files via upload

---
+++ day01.js
@@ -0,0 +1,82 @@
+// Activity 1: Variable Declaration
+
+// Task 1:
+ 
+// var a = 67;
+// console.log(a);
+
+// Task 2:
+
+// let b = "Hello World!";
+// console.log(b);
+
+// Activity 2: Constant Declaration
+
+// Task 3:
+
+// const c = false;
+// console.log(c);
+
+// Activity 3: Data types
+
+// Task 4:
+
+// let myNumber = 42;
+// let myString = "Hello, world!";
+// let myBoolean = true;
+// let myObject = { name: "Dan", age: 30 };
+// let myArray = [1, 2, 3];
+
+// console.log(typeof myNumber);  // number
+// console.log(typeof myString);  // string
+// console.log(typeof myBoolean); // boolean
+// console.log(typeof myObject);  // object
+// console.log(typeof myArray);   // object
+
+// Activity 4: Reassigning Variables
+
+// Task 5:
+
+// let d = "Initial value 78";
+// console.log(d);
+
+// d = "New value 98";
+// console.log(d);
+
+// Acitivity 5: Understanding const
+
+// Task 6:
+
+// const e = "Can't change this!";
+// console.log(e);
+
+// e = "Trying to change this..."; // This will throw an error
+// console.log(e);
+
+
+// Feature Assignment
+
+// let a = "hashima"
+// let b = 15
+// let c = true
+// let d = { letter: "a", letter1: "b", letter2: "c",letter3: "d"}
+// let e = [1,2,3,4]
+// console.log(c, typeof c)
+// console.log(a, typeof a)
+// console.log(b, typeof b)
+// console.log(d, typeof d)
+// console.log(e, typeof e)
+
+// Reassignmenet demo
+
+// let a = "hashima";
+//  a = "hasi"
+//  console.log(a)
+
+ 
+// const b = "hashima";
+// b = "hasi"
+// console.log(b)
+
+
+
