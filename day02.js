ca54981e8607db83a99e5bb03d34ee9adc39cc5a
Hashmeena_Farhan <devish8463@gmail.com>
2025-08-26 00:41:23 +0500

    Add files via upload

---
+++ day02.js
@@ -0,0 +1,113 @@
+// Acticity 1: Arithmatic Operations
+
+// Task 1:
+
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1 + num2;
+// console.log("The sum is: " + sum);
+
+// Task 2:
+
+// let num3 = 5;
+// let num4 = 10;
+// let subtraction = num3 - num4;
+// console.log("The subtraction is: " + subtraction);
+
+// Task 3:
+
+// let num5 = 5;
+// let num6 = 10;
+// let product = num5 * num6;
+// console.log("The product is: " + product);
+
+// Task 4:
+
+// let num7 = 5;
+// let num8 = 10;
+// let division = num7 / num8;
+// console.log("The division is: " + division);
+
+// Task 5:
+
+// let dividend = 17;
+// let divisor = 5;
+// let remainder = dividend % divisor;
+// console.log("The remainder is: " + remainder);
+
+// Activity 2: Assignment Operations
+
+// Task 6:
+
+// let myNum = 10;
+// myNum += 5; // This is the same as myNum = myNum + 5
+// console.log("The new value is: " + myNum);
+
+// Task 7:
+
+// let myNumber = 10;
+// myNumber -= 5; // This is the same as myNumber = myNumber + 5
+// console.log("The new value is: " + myNumber);
+
+// Acticity 3: Comparison Operator
+
+// Task 8:
+
+// let newvalue = 5 > 7;
+// console.log(newvalue); //false
+
+// let newval = 5 < 7;
+// console.log(newval); //true
+
+// Task 9:
+
+// let number1 = 8;
+// let number2 = 12;
+
+// console.log(number1 >= number2); // false
+// console.log(number1 <= number2); // true
+
+// Task 10:
+
+// let number1 = 10;
+// let number2 = "10";
+
+// console.log(number1 == number2); // true (loose equality)
+// console.log(number1 === number2); // false (strict equality)
+
+// Activity 4: Logical Operators
+
+// Task 11:
+
+// let number = 15;
+
+// // Checking if number is greater than 10 AND less than 20
+// let result = (number > 10) && (number < 20);
+
+// console.log(result); // true
+
+// Task 12:
+
+// let number = 5;
+
+// // Checking if number is less than 3 OR less than 10
+// let result = (number < 3) || (number < 10);
+
+// console.log(result); // true
+
+// Task 13:
+// let isRainy = true;
+
+// // Negating the condition
+// let isNotRainy = !isRainy;
+
+// console.log( isNotRainy); // false
+
+// Activity 5:Ternary Operator
+ 
+// Task 14:
+
+// let num = 8;
+// let output = (num >= 7) ? "Positive" : "Negative";
+
+// console.log(output); // "Positive"
