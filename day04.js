25429ec0e2b6bf6d80d7d85a768dee55bf6377a2
Hashmeena_Farhan <devish8463@gmail.com>
2025-08-28 22:58:56 +0500

    Add files via upload

---
+++ day04.js
@@ -0,0 +1,106 @@
+// Day 04 : Loops
+
+// Activity 1 : For Loop 
+// Task 1 : Write a program to print numbers from 1 to 10 using for loop.
+
+// for (let i = 1; i <=10; i++){
+//     console.log(i);
+// }
+
+// Task 2 : Write a program to calculate the sum of numbers from 1 to 10 using
+// a for loop
+
+// for (let i = 5; i <=50; i+=5){
+//          console.log(i);
+//  }
+
+// Activiy 2 : While Loop
+// Task 3 : Write a program to calculate the sum of numbers from 1 to 10
+// using a while loop.
+
+// let sum = 0;
+// let i = 1;
+
+// while ( i <= 10){
+//     sum += i;
+//     i++;
+// }
+// console.log(sum);
+
+// Task 4 : Write a program to print numbers from 10 to 1 using  a while loop
+
+// let i = 10
+// while (i >=1 ){
+//     console.log(i);
+//     i-=1;
+// }
+
+// Activity 3 : Do ... while loop
+// Task 5 : Write a program to print numbers from 1 to 5 using a do... while
+// loop.
+
+// let i = 1;
+//  do {
+//     console.log(i);
+//     i++;
+//  } while ( i <= 5)
+
+// Task 6 : write a program to calculate the factorial of a number using a 
+// do... while loop
+
+// let number = 5;
+// let factorial = 1;
+// let i = 1;
+
+// do {
+//     factorial *= i;
+//     i++;
+// } while (i <= number);
+
+// console.log(`The factorial of ${number} is: ${factorial}`);
+
+// Activity 4 : Nested Loops
+// Task 7 : Write a program to print a pattern using nested for loops
+
+// let rows = 5; // Number of rows in the pattern
+
+// Outer loop for rows
+// for (let i = 1; i <= rows; i++) {
+//     let pattern = '';
+
+//     // Inner loop for columns
+//     for (let j = 1; j <= i; j++) {
+//         pattern += '* '; // Add stars and spaces to form the pattern
+//     }
+
+//     console.log(pattern); // Print each row of the pattern
+// }
+
+// Activity 5 : Loop Control Statements 
+
+// Task 8: write a program to print numbers from 1 to 10 but skip the number 5
+// using the continue statement.
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) {
+//         continue; // Skip number 5 and continue with the next iteration
+//     }
+//     console.log(i);
+// }
+
+// Task 9: Write a program to print numbers from 1 to 10 , but stop teh loop
+// when the number is 7 using the break statement.
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 7) {
+//         break; // Stop the loop when i is 7
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+
+
